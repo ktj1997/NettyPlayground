1f320e1caad024cacfbfd75192a2da1233ca19bb
ktj1997 <rlaxowns7916@gmail.com>
2025-03-09 14:46:41 +0900

    test: EchoServer-IntegrationTest

---
+++ src/test/kotlin/com/example/netty/integration/TcpEchoServerTest.kt
@@ -0,0 +1,23 @@
+package com.example.netty.integration
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import java.net.Socket
+import kotlin.test.assertEquals
+
+@SpringBootTest
+class TcpEchoServerTest {
+
+    @Test
+    fun `Client는_요청을_Echo로_응답_받을_수_있다`(){
+        val expect = "hello"
+        val client = Socket("127.0.0.1", 3000)
+
+        client.use {
+            it.getOutputStream().write(expect.toByteArray())
+            val actual = it.getInputStream().readNBytes(expect.length)
+
+            assertEquals(expect, String(actual))
+        }
+    }
+}
